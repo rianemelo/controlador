323b7f81c32c8b8371e6ae8848fb5c0dd8e88ab8
unknown <rianemlo@gmail.com>
2024-08-08 00:43:13 -0300

    Criada parser para passar os referenciais para números módulo 4, facilitando virar para a direita ou esquerda; implementação da interface dos movimentos da sonda.

--- src/main/java/br/com/elo7/controlador/model/Sonda.java
+++ src/main/java/br/com/elo7/controlador/model/Sonda.java
@@ -1,5 +1,51 @@
 package br.com.elo7.controlador.model;
 
-public class Sonda {
+import br.com.elo7.controlador.model.parser.ReferencialParser;
+import br.com.elo7.controlador.service.SondaMovimento;
 
+public class Sonda implements SondaMovimento{
+
+	private Integer x;
+	private Integer y;
+	private String referencial;
+	
+	public Sonda(Integer x, Integer y, String referencial) {
+		this.x = x;
+		this.y = y;
+		this.referencial = referencial;
+	}
+	
+	
+	@Override
+	public void left(String direcao) {		
+		int nDirecao = ReferencialParser.get().numeroModulo(direcao);
+		int nReferencial = ReferencialParser.get().numeroModulo(direcao);
+		referencial = 
+				ReferencialParser.get().referencial(nDirecao + nReferencial);
+	
+	}
+	
+	@Override
+	public void right(String direcao) {
+		int nDirecao = ReferencialParser.get().numeroModulo(direcao);
+		int nReferencial = ReferencialParser.get().numeroModulo(direcao);
+		referencial = 
+				ReferencialParser.get().referencial(nDirecao - nReferencial);
+	}
+		
+	@Override
+	public void mover(String direcao) {
+		switch(direcao) {
+			case "S": y = y - 1;
+				break;
+			case "N": y = y + 1;
+				break;
+			case "E": x = x + 1;
+				break;
+			case "W": x = x - 1;
+				break;
+			default: //ainda não sei
+				break;
+		}
+	}
 }
---
+++ src/main/java/br/com/elo7/controlador/service/SondaMovimento.java
@@ -0,0 +1,11 @@
+package br.com.elo7.controlador.service;
+
+public interface SondaMovimento {
+
+	void left(String direcao);
+
+	void right(String direcao);
+
+	void mover(String direcao);
+
+}
